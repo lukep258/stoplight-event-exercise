38fe5f9aca725377dfa581e6352f7b0e3687b896
Luke Park <143543147+lukep258@users.noreply.github.com>
2023-10-12 15:37:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 (function() {
   'use strict';
-
   // YOUR CODE HERE
+
+  let stopButton = document.getElementsByTagName('stopButton');
+  let slowButton = document.getElementsByTagName('slowButton');
+  let goButton = document.getElementsByTagName('goButton');
+  let stopLight = document.getElementsByTagName('stopLight')[0];
+  console.log(stopLight);
+  let slowLight = document.getElementsByTagName('slowLight');
+  let goLight = document.getElementsByTagName('goLight');
+
+  const button = function(event){
+    console.log(event.target);
+    if(event.target.id === "stopButton"){
+      stopLight.classList.add(".stop");
+    }
+  }
+  window.addEventListener("click",button);
+
 })();
